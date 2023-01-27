3d3b6bfa27a92840936b7b2ffbba2a7f1c1ce046
Thalita dos Reis <dosreistha@gmail.com>
2023-01-27 15:17:11 +0100

    hide/show chat when joining the event

--- src/pages/EventDetails.js
+++ src/pages/EventDetails.js
@@ -3,73 +3,71 @@ import { useState, useEffect, useContext } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import { AuthContext } from '../context/auth.context';
 
-import authForAPI from '../utils/authForAPI';
 import ChatBox from '../components/ChatBox';
-// import JoinEvent from '../components/JoinEvent';
+import authForAPI from '../utils/authForAPI';
 import dayjs from 'dayjs';
 
 export default function EventDetails(props) {
 
-    const navigate = useNavigate();
-    const {user} = useContext(AuthContext);
-    const {eventId} = useParams();
-
-    const [event, setEvent] = useState([]);
-    const [participants, setParticipants] = useState([]);
-    const [toggle, setToggle] = useState(true);
-
-    const getEvent = () => {
-      axios
-        .get(`${process.env.REACT_APP_API_URL}/api/events/${eventId}`)
-        .then((response) => {
-            setEvent(response.data);
-          })
-        .catch((error) => console.log("Error getting event", error));
-    };
-
-    useEffect(()=> {
-      getEvent();
-    }, [] );
-
-    // get participants
-    const getParticipants = () => {
-      const requestBody = { userId: user._id };
-
-      axios
-        .put(`${process.env.REACT_APP_API_URL}/api/events/${eventId}/participants`, requestBody, authForAPI())
-        .then((response) => {
-            setParticipants(response.data);
-            getEvent()
-          })
-          .catch((error) => console.log("Error getting event", error));
-        }
-
-    // deleting the event
-    const deleteEvent = () => {
-      axios
-        .delete(`${process.env.REACT_APP_API_URL}/api/events/${eventId}`,authForAPI())
-        .then(() => {
-          navigate(`/events`)
-          props.editCallback()
+  const {user} = useContext(AuthContext);
+  const navigate = useNavigate();
+  const {eventId} = useParams();
+  
+  const [event, setEvent] = useState([]);
+  const [toggle, setToggle] = useState(true);
+  
+  const getEvent = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/api/events/${eventId}`)
+      .then((response) => {
+          setEvent(response.data);
         })
-        .catch((error) => console.log('Error deleting these details', error));
-    };
+      .catch((error) => console.log("Error getting event", error));
+  };
+  
+  useEffect(()=> {
+    getEvent();
+  }, [] );
 
-    // join event toggle button
-    const toggleEventChat = () => {
-      setToggle(!toggle)
-    };
+  // get participants
+  const getParticipants = () => {
+    const requestBody = { userId: user._id };
 
-    return (
-      <div style={{display: "flex", justifyContent: "center", marginTop: "50px" }}>
-        <div className='event-details-container' style={{width: "50%", border: "1px solid"}} >
+    axios
+      .put(`${process.env.REACT_APP_API_URL}/api/events/${eventId}/participants`, requestBody, authForAPI())
+      .then((response) => {
+        getEvent();
+      })
+      .catch((error) => console.log("Error getting event", error));
+  }
+      
+  // deleting the event
+  const deleteEvent = () => {
+    axios
+      .delete(`${process.env.REACT_APP_API_URL}/api/events/${eventId}`,authForAPI())
+      .then(() => {
+        navigate(`/events`)
+        props.editCallback()
+      })
+      .catch((error) => console.log('Error deleting these details', error));
+  };
+  
+  // join event toggle button
+  const toggleEventChat = () => {
+    setToggle(!toggle)
+  };
+
+  return (
+    <>
+      <div style={{display: "flex", justifyContent: "space-evenly", marginTop: "50px" }}>
+        <div className='event-details-container' style={{width: "30%", border: "1px solid"}} >
+          <h1>{event.title}</h1>
           <img src={event.image} alt="" style={{ margin: "auto", width: "auto", height: "350px" }} />
 
-          <h1>{event.title}</h1>
-          <h3>by: {event.author?.username}</h3>
           <p><b>Location:</b> {event.country} / {event.city}</p>
           <p><b>Date:</b> {dayjs(event.date).format("ddd DD MMM YYYY") }</p>
           <p><b>Description: </b>{event.description}</p>
+          <p><b>By:</b> {event.author?.username}</p>
 
           {/* only creator of the event can use the functionality edit/delete */}
           {event.author?.username === user?.username && 
@@ -80,19 +78,19 @@ export default function EventDetails(props) {
           }
 
         </div>
-        <div style={{width: "50%", textAlign: "start", border: "1px solid"}}>
-
+        <div style={{width: "50%", padding: "0 25px", textAlign: "start", border: "1px solid"}}>
           <h1>Attending (<b style={{color: "#f56457"}}>{event.participants?.length}</b>)</h1>
-          
-          <button onClick={getParticipants}>Join Event</button>
-          <ChatBox eventId={eventId} />
 
-          {/* <button onClick={toggleEventChat}>{toggle ? 'Hide Chat' : "Show Chat" }</button> 
-          {toggle && <JoinEvent eventId={eventId} />} */}
+          {event.participants?.includes(user._id)
+            ? <button onClick={toggleEventChat}>{toggle ? 'Hide Chat' : "Show Chat" }</button>
+            : <button onClick={getParticipants}>Join Event</button>
+          }
+            {toggle && <ChatBox eventId={eventId} /> }
 
         </div>
 
       </div>
-    );
-  }
+    </>
+  );
+}
   
